4221516ebee380b294e62105677f7d96e319498a
KamilKopczyk <kamilkopczyk@o2.pl>
2024-12-11 14:17:42 +0100

    Add files via upload

---
+++ Lab-01/Zadania od 1-4/LAB_01/LAB_01.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int liczba;
+float l1, l2, l11,l22,l33,l44,sa;
+
+int main() {
+
+	//Zadanie 1
+
+	/*
+	printf("Numer studenta: 134927 \n");
+	*/
+
+	//Zadanie 2
+
+	/*
+
+	printf("Podaj liczbe calkowita: ");
+	scanf_s("%d",&liczba);
+
+	if (liczba % 2 == 0) {
+
+		printf("Liczba parzysta \n");
+
+
+	}
+	else {
+
+		printf("Liczba nieparzysta \n");
+
+
+	}
+	*/
+
+	//Zadanie 3
+
+	/*printf("Podaj 1 liczbe zmiennoprzecinkowa: ");
+	scanf_s("%f", &l1);
+	printf("Podaj 2 liczbe zmiennoprzecinkowa: ");
+	scanf_s("%f", &l2);
+
+	if (l1 > l2) {
+
+		printf("Liczba %f jest wieksza od %f", l1, l2 \n);
+
+	}
+	else if (l2 > l1) {
+	
+		printf("Liczba %f jest wieksza od %f", l2, l1 \n);
+	
+	}
+	else {
+	
+	
+		printf("Liczba %f jest rowna liczbie %f", l1, l2 \n);
+	
+	}*/
+
+	//Zadanie 4
+
+	/*printf("Podaj 1 liczbe zmiennoprzecinkowa: ");
+	scanf_s("%f", &l11);
+	printf("Podaj 2 liczbe zmiennoprzecinkowa: ");
+	scanf_s("%f", &l22);
+	printf("Podaj 3 liczbe zmiennoprzecinkowa: ");
+	scanf_s("%f", &l33);
+	printf("Podaj 4 liczbe zmiennoprzecinkowa: ");
+	scanf_s("%f", &l44);
+
+	sa = (l11 + l22 + l33 + l44) / 4;
+
+	printf("Srednia arytmetyczna liczb: %f,%f,%f,%f = %f \n",l11,l22,l33,l44,sa);*/
+
+	
+	return 0;
+
+}
